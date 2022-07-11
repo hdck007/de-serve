4a474b912cc0941520c4f22f0b84cdbb940a34e4
hari <haridarshanofficial@gmail.com>
2022-07-11 10:00:36 +0530

    updated the home page

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,7 +1,12 @@
-import { useContractReader } from "eth-hooks";
+import { Button, Card } from "antd";
+import { useContractLoader } from "eth-hooks";
 import { ethers } from "ethers";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
+const someArray = new Array(20).fill(0);
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -9,114 +14,69 @@ import { Link } from "react-router-dom";
  * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
  * @returns react component
  **/
-function Home({ yourLocalBalance, readContracts }) {
-  // you can also use hooks locally in your component of choice
-  // in this case, let's keep track of 'purpose' variable from our contract
-  const purpose = useContractReader(readContracts, "YourContract", "purpose");
+function Home({ customContract, provider, chainId, contractConfig, name }) {
+  const [surveys, setSurveys] = useState([]);
+
+  const contracts = useContractLoader(provider, contractConfig, chainId);
+  let contract;
+  if (!customContract) {
+    contract = contracts ? contracts[name] : "";
+  } else {
+    contract = customContract;
+  }
+
+  useEffect(() => {
+    if (!contract) return;
+    contract.getAllSurveys().then(data => setSurveys(data));
+  }, [contracts]);
 
   return (
-    <div>
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>📝</span>
-        This Is Your App Home. You can start editing it in{" "}
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+    <div
+      style={{
+        marginTop: "1rem",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        rowGap: "1rem",
+      }}
+    >
+      {surveys.map((item, index) => (
+        <div
+          style={{
+            padding: "10px",
+            width: "200px",
+            height: "200px",
+            borderRadius: "10px",
+            boxShadow: "1px 2px 3px rgba(0,0,0,0.1)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
         >
-          packages/react-app/src/views/Home.jsx
-        </span>
-      </div>
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>✏️</span>
-        Edit your smart contract{" "}
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          YourContract.sol
-        </span>{" "}
-        in{" "}
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          packages/hardhat/contracts
-        </span>
-      </div>
-      {!purpose ? (
-        <div style={{ margin: 32 }}>
-          <span style={{ marginRight: 8 }}>👷‍♀️</span>
-          You haven't deployed your contract yet, run
-          <span
-            className="highlight"
+          <h3
             style={{
-              marginLeft: 4,
-              /* backgroundColor: "#f9f9f9", */ padding: 4,
-              borderRadius: 4,
-              fontWeight: "bolder",
+              fontWeight: "bold",
+              width: "100%",
             }}
           >
-            yarn chain
-          </span>{" "}
-          and{" "}
-          <span
-            className="highlight"
+            {item.title}
+          </h3>
+          <div
             style={{
-              marginLeft: 4,
-              /* backgroundColor: "#f9f9f9", */ padding: 4,
-              borderRadius: 4,
-              fontWeight: "bolder",
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: "1rem",
             }}
           >
-            yarn deploy
-          </span>{" "}
-          to deploy your first contract!
+            <Button>{Number(item.participants._hex)}</Button>
+            <Button>
+              <Link to={`survey/${index}`}>Check</Link>
+            </Button>
+          </div>
         </div>
-      ) : (
-        <div style={{ margin: 32 }}>
-          <span style={{ marginRight: 8 }}>🤓</span>
-          The "purpose" variable from your contract is{" "}
-          <span
-            className="highlight"
-            style={{
-              marginLeft: 4,
-              /* backgroundColor: "#f9f9f9", */ padding: 4,
-              borderRadius: 4,
-              fontWeight: "bolder",
-            }}
-          >
-            {purpose}
-          </span>
-        </div>
-      )}
-
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>🤖</span>
-        An example prop of your balance{" "}
-        <span style={{ fontWeight: "bold", color: "green" }}>({ethers.utils.formatEther(yourLocalBalance)})</span> was
-        passed into the
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          Home.jsx
-        </span>{" "}
-        component from
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          App.jsx
-        </span>
-      </div>
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>💭</span>
-        Check out the <Link to="/hints">"Hints"</Link> tab for more tips.
-      </div>
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>🛠</span>
-        Tinker with your smart contract using the <Link to="/debug">"Debug Contract"</Link> tab.
-      </div>
+      ))}
     </div>
   );
 }
